55183a4ce77a4a95e45304afb371dc036f2f6b09
GiselleLop <giselleLop@users.noreply.github.com>
2024-04-09 20:42:54 -0500

    maquetacion terminada

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ function capsules() {
   fetch('https://api.spacexdata.com/v3/capsules')
   .then(data => data.json())
   .then(resp => {
-    console.log(resp)
+    console.log(resp);
     resp.forEach(element => {
+      let imageStatus = './images/rocketActive.svg';
+      let textElement = element.details
+      if (element.details === null) {
+        textElement = '';
+      }
+      if(element.status === "destroyed" || element.status === "retired") {
+        imageStatus = './images/rocketDeactivate.svg'
+      }
+      if(element.status === "unknown") {
+        imageStatus = './images/rocketUnknown.svg'
+      }
       const elementContainer = document.createElement('div')
       elementContainer.classList = 'elementContainer'
       elementContainer.innerHTML = `
       <h1> ${element.capsule_serial} </h1>
-      <p> ${element.details} </p>
+      <img src='${imageStatus}'>
+      <p> ${textElement} </p>
       `
       ContainerJSON.append(elementContainer)
     });
